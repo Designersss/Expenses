f4f785d24dbaaf06ef9aee3e33dbc99e60df274b
Designersss <cheredkovden3@gmail.com>
2023-03-04 19:41:49 +0300

    create Context and start project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {user} from "./store/userStore.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export const Context = createContext(null)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Context.Provider value={{
+        user: user
+    }}>
+        <App/>
+    </Context.Provider>
+);
---
+++ src/store/userStore.js
@@ -0,0 +1,6 @@
+export const user = {
+    id: 0,
+    Cash: 0,
+    name: '',
+    expenses: 0
+}
