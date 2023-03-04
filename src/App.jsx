23da58485f9a926cf85d22b0d7f8da3e96a74643
Designersss <cheredkovden3@gmail.com>
2023-03-04 20:29:04 +0300

    Fix object

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,45 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import {Context} from "./main.jsx";
 import {observer} from "mobx-react";
+import {transaction} from "mobx";
 
-const App = observer(() => {
+const App = () => {
     const {user} = useContext(Context)
     const [addCash, setAddCash] = useState('')
     const [expenses, setExpenses] = useState(0)
     const [name, setName] = useState('')
     const [cash, setCash] = useState(user.cash)
+    const [amount, setAmount] = useState(user.transaction)
+    const newAmount = {
+        id: +1,
+        amount: 0,
+    }
     const addCashFunc = () => {
+        user.transaction.amount = Number(addCash) // Finish transaction amount OBJECT
         setCash(user.cash += Number(addCash))
         setAddCash('')
+        console.log(amount)
     }
     const removeCashFunc = () => {
         setCash(user.cash -= Number(addCash))
         setAddCash('')
     }
-    console.log(user.cash)
     return (
         <div className="App">
+            <div className='block'>
+                {user.cash}
+            </div>
             <input type="text" value={addCash} onChange={e => setAddCash(e.target.value)} placeholder='Введите сумму'/>
             <button onClick={addCashFunc}>Добавить в кошелек</button>
             <button onClick={removeCashFunc}>Вычет</button>
             <div className='block mt-20'>
-                {cash}
+                {amount.map(cash =>
+                    <div key={+1}> {cash.amount}</div>
+                )}
             </div>
         </div>
     )
-})
+}
 
 // cash and expenses ??? +- ??? amount == cash || expenses
 
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,10 +1,7 @@
 export const user = {
     id: 0,
-    cash: 12,
+    cash: 0,
     name: '',
     expenses: 0,
-    transaction: {
-        text: '',
-        amount: 0
-    }
+    transaction: []
 }
