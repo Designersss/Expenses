6c50e208057a197db2a79d46fa4e0c2433f06068
Designersss <cheredkovden3@gmail.com>
2023-03-04 20:07:09 +0300

    add cash and remove cash

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import {useContext, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import {Context} from "./main.jsx";
+import {observer} from "mobx-react";
 
-function App() {
-  const {user} = useContext(Context)
-  console.log(user)
-  return (
-    <div className="App">
-    </div>
-  )
-}
+const App = observer(() => {
+    const {user} = useContext(Context)
+    const [addCash, setAddCash] = useState('')
+    const [expenses, setExpenses] = useState(0)
+    const [name, setName] = useState('')
+    const [cash, setCash] = useState(user.cash)
+    const addCashFunc = () => {
+        setCash(user.cash += Number(addCash))
+        setAddCash('')
+    }
+    const removeCashFunc = () => {
+        setCash(user.cash -= Number(addCash))
+        setAddCash('')
+    }
+    console.log(user.cash)
+    return (
+        <div className="App">
+            <input type="text" value={addCash} onChange={e => setAddCash(e.target.value)} placeholder='Введите сумму'/>
+            <button onClick={addCashFunc}>Добавить в кошелек</button>
+            <button onClick={removeCashFunc}>Вычет</button>
+            <div className='block mt-20'>
+                {cash}
+            </div>
+        </div>
+    )
+})
+
+// cash and expenses ??? +- ??? amount == cash || expenses
 
 export default App
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,6 +1,10 @@
 export const user = {
     id: 0,
-    Cash: 0,
+    cash: 12,
     name: '',
-    expenses: 0
+    expenses: 0,
+    transaction: {
+        text: '',
+        amount: 0
+    }
 }
